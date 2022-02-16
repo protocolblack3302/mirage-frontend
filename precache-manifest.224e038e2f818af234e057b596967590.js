28304db3cd620bf23e13325cbd42a3013173b0c6
protocolblack <protocolblack3302@gmail.com>
2022-02-17 05:04:14 +0530

    first commit

---
+++ precache-manifest.224e038e2f818af234e057b596967590.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d143919e7affc63fb620edb406e0dd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "132f282fe958f4052dce",
+    "url": "/static/css/2.f3112b35.chunk.css"
+  },
+  {
+    "revision": "4666655651d889689393",
+    "url": "/static/css/main.de79f742.chunk.css"
+  },
+  {
+    "revision": "132f282fe958f4052dce",
+    "url": "/static/js/2.27139745.chunk.js"
+  },
+  {
+    "revision": "42493d892feb3728c40c7550443741a7",
+    "url": "/static/js/2.27139745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8032b5359bc4900af5f6",
+    "url": "/static/js/3.54ff5303.chunk.js"
+  },
+  {
+    "revision": "4666655651d889689393",
+    "url": "/static/js/main.2057ffe8.chunk.js"
+  },
+  {
+    "revision": "ac773d766f275e23fbb0",
+    "url": "/static/js/runtime-main.8f15a822.js"
+  },
+  {
+    "revision": "db60f1f40c71648f3997d38a9e219113",
+    "url": "/static/media/ProcessingGreen.db60f1f4.svg"
+  },
+  {
+    "revision": "de99a143f49038dfac1b9cf38934e2a3",
+    "url": "/static/media/Search.de99a143.svg"
+  },
+  {
+    "revision": "4760643702cb5570427bbbc116f793c1",
+    "url": "/static/media/city.47606437.jpg"
+  },
+  {
+    "revision": "ff44632a7f86c9ba094133621790db56",
+    "url": "/static/media/fault.ff44632a.png"
+  },
+  {
+    "revision": "1451772f2edc303d269d1d2ae7905f36",
+    "url": "/static/media/moon.1451772f.png"
+  }
+]);
